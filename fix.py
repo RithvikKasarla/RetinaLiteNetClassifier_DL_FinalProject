26112275c97c31c9fb9c51eeffc2ea0e8d6617a4
RithvikKasarla <rithvikrk719@gmail.com>
2025-12-01 14:32:25 -0500

    fixed

--- fix.py
+++
@@ -1,5 +0,0 @@
-import json, pathlib
-p = pathlib.Path("RetinaLiteClassifier.ipynb")
-nb = json.loads(p.read_text())
-nb.get("metadata", {}).pop("widgets", None)   # drop the bad block
-p.write_text(json.dumps(nb, indent=2))
